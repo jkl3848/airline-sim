f8723ba68e23073bdc58c20c6ce823c10a71fe7b
Jachin Lamoreaux <jachin@lamfam.net>
2025-06-01 19:41:50 -0400

    volt and tailwind basics

--- vite.config.js
+++ vite.config.js
@@ -6,15 +6,18 @@ import vueDevTools from "vite-plugin-vue-devtools";
 import ReactivityTransform from "@vue-macros/reactivity-transform/vite";
 import json5Plugin from 'vite-plugin-json5'
 
+import tailwindcss from '@tailwindcss/vite'
+
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueDevTools(), ReactivityTransform(), json5Plugin()],
+  plugins: [vue(), vueDevTools(), ReactivityTransform(), tailwindcss(), json5Plugin()],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
       "@modules": fileURLToPath(new URL("./src/modules", import.meta.url)),
       "@stores": fileURLToPath(new URL("./stores", import.meta.url)),
       "@data": fileURLToPath(new URL("./lib/data", import.meta.url)),
+      "@volt": fileURLToPath(new URL("./src/volt", import.meta.url)),
     },
   },
 });
